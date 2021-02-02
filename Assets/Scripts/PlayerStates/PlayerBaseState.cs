beb6e8faf2e44c2b79ca75737f86e01b39340a72
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-02-02 13:16:37 +0100

    Implement character movement with walking animations

---
+++ Assets/Scripts/PlayerStates/PlayerBaseState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public abstract class PlayerBaseState
+{
+    protected PlayerController player;
+    public virtual void EnterState(PlayerController player)
+    {
+        this.player = player;
+    }
+
+    public abstract void Update();
+
+    // TODO: I don't like I am force to implement this when not all the subclasses will use
+    public abstract void FixedUpdate();
+
+    public abstract void OnCollisionEnter();
+}
+
---
+++ Assets/Scripts/PlayerStates/PlayerIdleState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState
+{
+    public override void EnterState(PlayerController player)
+    {
+        base.EnterState(player);
+        // Transition Animator to Idle (See Animator states graph)
+        player.Animator.SetBool("IsWalking", false);
+    }
+
+    public override void Update()
+    {
+        // If any Input direction, transition to walking state
+        if (Input.GetAxisRaw("Horizontal") != 0.0f || 
+            Input.GetAxisRaw("Vertical") != 0.0f)
+        {
+            player.TransitionToState(player.WalkState);
+        }
+
+        // TODO: Add swingSword Input and transition to swing-sword state
+    }
+
+    public override void OnCollisionEnter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FixedUpdate()
+    {
+
+    }
+
+}
