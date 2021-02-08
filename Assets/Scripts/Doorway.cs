51ba0287fd706e1b1882de87cacf9a1ae8f90954
Gerard Cuello <fontinixxl@users.noreply.github.com>
2021-02-08 16:42:43 +0100

    Add some code to test the doorway (open and close)
    Pressing O will open/close the four doorways. If opend the player can
    cross and will be teletransport to the center.

---
+++ Assets/Scripts/Doorway.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class Doorway : MonoBehaviour
+{
+    public Sprite spriteDoorOpen;
+    public Sprite spriteDoorClosed;
+
+    public enum Direction
+    {
+        Top, Right, Bottom, Left
+    }
+    public Direction direction;
+    private bool isOpen;
+
+    private SpriteRenderer spriteRenderer ;
+    private int _horizontalTiles;
+    private int _verticalTiles;
+
+    private void Awake()
+    {
+        // TODO: Delete! Get it from the RoomManager
+        PixelPerfectCamera pixPerfCam = Camera.main.GetComponent<PixelPerfectCamera>();
+        _horizontalTiles = (pixPerfCam.refResolutionX / pixPerfCam.assetsPPU) - 2;
+        _verticalTiles = (pixPerfCam.refResolutionY / pixPerfCam.assetsPPU) - 2;
+        // End delete
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isOpen = false;
+        SetPosition();
+    }
+
+    private void SetPosition()
+    {
+        float x, y;
+        if (direction == Direction.Bottom)
+        {
+            x = _horizontalTiles * 0.5f;
+            y = 0f;
+        }
+        else if (direction == Direction.Left)
+        {
+            x = 0;
+            y = _verticalTiles * 0.5f;
+        }
+        else if (direction == Direction.Top)
+        {
+            x = _horizontalTiles * 0.5f;
+            y = _verticalTiles;
+        }
+        else
+        {
+            // Right
+            x = _horizontalTiles;
+            y = _verticalTiles * 0.5f;
+        }
+
+        transform.position = new Vector3(x, y, 0f);
+    }
+
+    private void Update()
+    {
+        // TODO: The logic must be moved to an event Listener
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            isOpen = !isOpen;
+            spriteRenderer.sprite = !isOpen ? spriteDoorClosed : spriteDoorOpen;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && isOpen)
+        {
+            collision.transform.position = new Vector3( _horizontalTiles/2 + 1, _verticalTiles/2 + 1, 0f);
+        }
+    }
+}
--- Assets/Scripts/World/RoomManager.cs
+++ Assets/Scripts/World/RoomManager.cs
@@ -6,172 +6,141 @@ using Random = UnityEngine.Random;
 
 public class RoomManager : MonoBehaviour
 {
-	[Serializable]
-	public class Count
-	{
-		public int minimum;
-		public int maximum;
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
 
-		public Count(int min, int max)
-		{
-			minimum = min;
-			maximum = max;
-		}
-	}
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
 
     private int virtualWith;
     private int virtualHight;
 
-    private int horizontalTiles;
-	private int verticalTiles;
+    [HideInInspector]
+    public int HorizontalTiles { get; private set; }
+    [HideInInspector]
+    public int VerticalTiles { get; private set; }
 
-	//Arrays of tile prefabs.
-	public GameObject[] floorTiles;
-	public GameObject[] topWallsTiles;
-	public GameObject[] bottomWallsTiles;
-	public GameObject[] leftWallsTiles;
-	public GameObject[] rightWallsTiles;
-	public GameObject topLeftCornerTile;
-	public GameObject topRightCornerTile;
-	public GameObject bottomLeftCornerTile;
-	public GameObject bottomRightCornerTile;
+    //Arrays of tile prefabs.
+    public GameObject[] floorTiles;
+    public GameObject[] topWallsTiles;
+    public GameObject[] bottomWallsTiles;
+    public GameObject[] leftWallsTiles;
+    public GameObject[] rightWallsTiles;
+    public GameObject topLeftCornerTile;
+    public GameObject topRightCornerTile;
+    public GameObject bottomLeftCornerTile;
+    public GameObject bottomRightCornerTile;
 
-	// PlayerPrefab
-	public GameObject player;
+    // PlayerPrefab
+    public GameObject player;
 
-	// DoorWays
-	public GameObject doorWayLeft;
-	public GameObject doorWayRight;
-	public GameObject doorWayTop;
-	public GameObject doorWayBottom;
-	private readonly string[] doorWayDirections = new string[] { "Left", "Right", "Top", "Bottom" };
+    // DoorWays
+    public List<Doorway> DoorwayPrefabs;
+    private List<Doorway> _doorways;
 
-	//A variable to store a reference to the transform of our Room object.
-	private Transform roomHolder;
+    //A variable to store a reference to the transform of our Room object.
+    private Transform roomHolder;
 
-	//A list of possible locations to place tiles.
-	private List<Vector3> gridPositions = new List<Vector3>();
-
+    //A list of possible locations to place tiles.
+    private List<Vector3> gridPositions = new List<Vector3>();
 
     private void Awake()
     {
-		PixelPerfectCamera pixPerfCam = Camera.main.GetComponent<PixelPerfectCamera>();
-		virtualWith = pixPerfCam.refResolutionX;
-		virtualHight = pixPerfCam.refResolutionY;
-		horizontalTiles = (virtualWith / pixPerfCam.assetsPPU) - 2;
-		verticalTiles = (virtualHight / pixPerfCam.assetsPPU) - 2;
-        //Debug.LogFormat("horizontalTiles {0} verticalTiles {1}", horizontalTiles, verticalTiles);
+        PixelPerfectCamera pixPerfCam = Camera.main.GetComponent<PixelPerfectCamera>();
+        virtualWith = pixPerfCam.refResolutionX;
+        virtualHight = pixPerfCam.refResolutionY;
+        HorizontalTiles = (virtualWith / pixPerfCam.assetsPPU) - 2;
+        VerticalTiles = (virtualHight / pixPerfCam.assetsPPU) - 2;
 
-	}
+        _doorways = new List<Doorway>();
+        //Debug.LogFormat("horizontalTiles {0} verticalTiles {1}", horizontalTiles, verticalTiles);
+    }
 
     private void Start()
     {
         GenerateWallsAndFloors();
-		GenerateDoorWays();
-		Instantiate(player, player.transform.position, Quaternion.identity);
+        GenerateDoorways();
+        Instantiate(player, player.transform.position, Quaternion.identity);
     }
+
     /*
-	 * Generates the walls and floors of the room, randomizing the various varieties
+	 * Generate the walls and floor of the room, randomizing the various varieties
 	 * of said tiles for visual variety.
 	 */
     void GenerateWallsAndFloors()
     {
-		//Instantiate Board and set boardHolder to its transform.
-		roomHolder = new GameObject("Room").transform;
+        //Instantiate Board and set boardHolder to its transform.
+        roomHolder = new GameObject("Room").transform;
 
-        for (int y = 1; y <= verticalTiles; y++)
+        for (int y = 1; y <= VerticalTiles; y++)
         {
-            for (int x = 1; x <= horizontalTiles; x++)
+            for (int x = 1; x <= HorizontalTiles; x++)
             {
                 GameObject toInstantiate;
 
-				// Corner tiles
+                // Corner tiles
                 if (x == 1 && y == 1)
                 {
-					toInstantiate = bottomLeftCornerTile;
+                    toInstantiate = bottomLeftCornerTile;
                 }
-				else if (x == 1 && y == verticalTiles)
+                else if (x == 1 && y == VerticalTiles)
                 {
-					toInstantiate = topLeftCornerTile;
+                    toInstantiate = topLeftCornerTile;
                 }
-				else if (x == horizontalTiles && y == 1)
+                else if (x == HorizontalTiles && y == 1)
                 {
-					toInstantiate = bottomRightCornerTile;
+                    toInstantiate = bottomRightCornerTile;
                 }
-				else if ( x == horizontalTiles && y == verticalTiles)
+                else if (x == HorizontalTiles && y == VerticalTiles)
                 {
-					toInstantiate = topRightCornerTile;
+                    toInstantiate = topRightCornerTile;
                 }
-				//random left - hand walls, right walls, top, bottom
-				else if (x == 1)
+                //random left - hand walls, right walls, top, bottom
+                else if (x == 1)
                 {
-					toInstantiate = leftWallsTiles[Random.Range(0, leftWallsTiles.Length)];
+                    toInstantiate = leftWallsTiles[Random.Range(0, leftWallsTiles.Length)];
                 }
-				else if (x == horizontalTiles)
+                else if (x == HorizontalTiles)
                 {
-					toInstantiate = rightWallsTiles[Random.Range(0, rightWallsTiles.Length)];
+                    toInstantiate = rightWallsTiles[Random.Range(0, rightWallsTiles.Length)];
                 }
-				else if (y == 1)
+                else if (y == 1)
                 {
-					toInstantiate = bottomWallsTiles[Random.Range(0, topWallsTiles.Length)];
+                    toInstantiate = bottomWallsTiles[Random.Range(0, topWallsTiles.Length)];
                 }
-				else if (y == verticalTiles)
+                else if (y == VerticalTiles)
                 {
-					toInstantiate = topWallsTiles[Random.Range(0, bottomWallsTiles.Length)];
+                    toInstantiate = topWallsTiles[Random.Range(0, bottomWallsTiles.Length)];
                 }
-				// if it's not a corner or a wall tile, be it a floor tile
+                // if it's not a corner or a wall tile, be it a floor tile
                 else
                 {
-					toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+                    toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
                 }
 
                 GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity);
-				instance.transform.SetParent(roomHolder);
-			}
-		}
-	}
-
-	// TODO: Extract to DoorWay Class
-	void GenerateDoorWays()
+                instance.transform.SetParent(roomHolder);
+            }
+        }
+    }
+    void GenerateDoorways()
     {
-		float x, y;
-		GameObject instance;
-		GameObject doorWay;
+        Transform doorWayHolder = new GameObject("DoorWays").transform;
+        doorWayHolder.SetParent(roomHolder);
 
-		Transform doorWayHolder = new GameObject("DoorWay").transform;
-		doorWayHolder.SetParent(roomHolder);
-
-		foreach (var direction in doorWayDirections)
+        foreach (var doorway in DoorwayPrefabs)
         {
-			switch (direction)
-			{
-				case "Left":
-					x = 0;
-					y = verticalTiles * 0.5f;
-					doorWay = doorWayLeft;
-					break;
-				case "Right":
-					x = horizontalTiles;
-					y = verticalTiles * 0.5f;
-					doorWay = doorWayRight;
-					break;
-				case "Top":
-					x = horizontalTiles * 0.5f;
-					y = verticalTiles;
-					doorWay = doorWayTop;
-					break;
-				case "Bottom":
-					x = horizontalTiles * 0.5f;
-					y = 0;
-					doorWay = doorWayBottom;
-					break;
-				default:
-					Debug.LogErrorFormat("DoorWay ''{0}'' Prefab doesn't exist!", direction);
-					return;
-			}
+            Doorway instance = Instantiate(doorway);
+            instance.transform.SetParent(doorWayHolder);
 
-			instance = Instantiate(doorWay, new Vector3(x, y, 0f), Quaternion.identity);
-			instance.transform.SetParent(doorWayHolder);
-		}
+            _doorways.Add(instance);
+        }
     }
 }
